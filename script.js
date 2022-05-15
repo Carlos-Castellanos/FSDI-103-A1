fb74fa0e69bc76d5bfad76f0730764fc8eb76091
Carlos-Castellanos <carloslopez@itmexicali.edu.mx>
2022-05-15 11:47:57 -0700

    Doing operation with variables
    working with: <p>salary (annual): $ <b>${monthSalary * 12}</b></p>

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* introduction */
+
 //alert("hello word");
 //Prompt("Enter your name: ")
 //console.log("this is a simple message");
@@ -18,9 +20,31 @@ document.writeln("<p> taxes:" +(salary*taxes)/100+"</p>");
 document.writeln(`taxes: (${salary}*${taxes})/100`);
 document.writeln(`<p>taxes: ${salary*taxes/100}</p>`);
 */
-var jobTitle = "engineer";
-var geoLocation = "Mexicali";
-var partnerName = "Lourdes";
-var numberChildren = 3;
+
+
+/* exercise #1 */
+/*
+let jobTitle = "engineer";
+let geoLocation = "Mexicali";
+let partnerName = "Lourdes";
+let numberChildren = 3;
 
 document.writeln(`<p>My job title is ${jobTitle} in ${geoLocation}</p> <p>I married with ${partnerName} and we have ${numberChildren} children</p>`);
+*/
+
+/* exercise #2 */
+let completeName = "Carlos Lopez";
+let emailAddress = "carloslopez@gmail.com";
+let age = 51;
+let password = "qwerty12345";
+let contry = "Mexico";
+let monthSalary = 2500;
+
+document.writeln(`
+    <p> Name: <b>${completeName}</b></p>
+    <p> email: <b>${emailAddress}</b> </p>
+    <p>salary (annual): $ <b>${monthSalary * 12}</b></p> 
+    
+`);
+document.write("Salario mensual: "+monthSalary.toLocaleString('en-IN', {style: 'currency',currency: 'INR', minimumFractionDigits: 2}));
+
