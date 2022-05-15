79eaeea3bebf4775e6058de96389e05b43214a9c
Carlos-Castellanos <carloslopez@itmexicali.edu.mx>
2022-05-15 11:25:13 -0700

    creating varibales
    we were creating variables with var and let

---
+++ script.js
@@ -0,0 +1,26 @@
+//alert("hello word");
+//Prompt("Enter your name: ")
+//console.log("this is a simple message");
+//console.warn("this is a warning message");
+//console.error("this is a error mesaage");
+// ` 96
+//variables
+/*var x;
+x=10;
+x=20;
+console.log(x);
+let salary="2000";
+const taxes=11;
+console.log((salary*taxes)/100);
+document.writeln(salary);
+document.writeln(taxes);
+document.writeln("<p> taxes:" +(salary*taxes)/100+"</p>");
+document.writeln(`taxes: (${salary}*${taxes})/100`);
+document.writeln(`<p>taxes: ${salary*taxes/100}</p>`);
+*/
+var jobTitle = "engineer";
+var geoLocation = "Mexicali";
+var partnerName = "Lourdes";
+var numberChildren = 3;
+
+document.writeln(`<p>My job title is ${jobTitle} in ${geoLocation}</p> <p>I married with ${partnerName} and we have ${numberChildren} children</p>`);
